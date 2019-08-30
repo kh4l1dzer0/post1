bf74c748fd2d7d6845740794d4cef892f891e6a7
kh4l1dzer0 <46495208+kh4l1dzer0@users.noreply.github.com>
2019-08-30 15:27:54 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2019/08/how-to-cook-nasi-goreng.html"}
-if(x=="2"){xxx="/2019/08/20-hal-yang-perlu-diketahui-setiap.html"}
-if(x=="3"){xxx="/2019/08/choose-good-company-name.html"}
-if(x=="4"){xxx="/2019/08/10-easy-tips-to-help-you-profit-from.html"}
-if(x=="5"){xxx="/2019/08/deretan-aplikasi-yang-harus-segera-di.html"}a_to_ve[i].href="https://shortdulu.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="08/how-to-cook-nasi-goreng.html"}
+if(x=="2"){xxx="08/20-hal-yang-perlu-diketahui-setiap.html"}
+if(x=="3"){xxx="08/choose-good-company-name.html"}
+if(x=="4"){xxx="8/10-easy-tips-to-help-you-profit-from.html"}
+if(x=="5"){xxx="8/deretan-aplikasi-yang-harus-segera-di.html"}a_to_ve[i].href="https://shortdulu.blogspot.com/2019/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
